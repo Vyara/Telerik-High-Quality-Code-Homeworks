bfaf15d4ea921e8a893079e991458992a3ca87f0
vyara <vyara_hristova@yahoo.de>
2015-08-09 23:46:14 +0300

    initial commit

---
+++ 10. Code Tuning and Optimization/AdvancedMaths/AdvancedMathsTests.cs
@@ -0,0 +1,66 @@
+﻿namespace AdvancedMaths
+{
+    using System;
+    using System.Diagnostics;
+
+    public class AdvancedMathsTests
+    {
+       private const int LoopingCount = 5000000;
+
+        internal static void Main()
+        {
+            Console.WriteLine("float");
+            float x1 = 0;
+            ExecuteMathTests(x1);
+
+            Console.WriteLine("double");
+            double x2 = 0;
+            ExecuteMathTests(x2);
+
+            Console.WriteLine("decimal");
+            decimal x3 = 0;
+            ExecuteMathTests(x3);
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        private static void ExecuteMathTests(dynamic a)
+        {
+            a = a + 1;
+
+            Console.Write("Square Root: ");
+            DisplayExecutionTime(() =>
+            {
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    Math.Sqrt((double)a);
+                }
+            });
+
+            Console.Write("Natural algorithm: ");
+            DisplayExecutionTime(() =>
+            {
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    Math.Log((double)a);
+                }
+            });
+
+            Console.Write("Sinus: ");
+            DisplayExecutionTime(() =>
+            {
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    Math.Sin((double)a);
+                }
+            });
+        }
+    }
+}
---
+++ 10. Code Tuning and Optimization/SimpleMaths/SimpleMathsTests.cs
@@ -0,0 +1,96 @@
+﻿namespace SimpleMaths
+{
+    using System;
+    using System.Diagnostics;
+
+    public class SimpleMathsTests
+    {
+        private const int LoopingCount = 5000000;
+
+        internal static void Main()
+        {
+            Console.WriteLine("Int");
+            var x1 = 0;
+            ExecuteMathTests(x1);
+
+            Console.WriteLine("Long");
+            long x2 = 0;
+            ExecuteMathTests(x2);
+
+            Console.WriteLine("float");
+            float x3 = 0;
+            ExecuteMathTests(x3);
+
+            Console.WriteLine("double");
+            double x4 = 0;
+            ExecuteMathTests(x4);
+
+            Console.WriteLine("decimal");
+            decimal x5 = 0;
+            ExecuteMathTests(x5);
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        private static void ExecuteMathTests(dynamic a)
+        {
+
+            Console.Write("Add: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 0;
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    a = a + 1;
+                }
+            });
+
+            Console.Write("Subtract: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 5000000;
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    a = a - 1;
+                }
+            });
+
+            Console.Write("Increment: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 0;
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    a++;
+                }
+            });
+
+            Console.Write("Multiply: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 1;
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    a = a * 1;
+                }
+            });
+
+            Console.Write("Divide: ");
+            DisplayExecutionTime(() =>
+            {
+                a = a + 1;
+                for (var i = 0; i < LoopingCount; i++)
+                {
+                    a = a / 1;
+                }
+            });
+        }
+    }
+}
